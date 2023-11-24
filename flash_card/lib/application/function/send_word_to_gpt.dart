4bca39cea571581f439b3bf09a5d57b7fe418e0f
oguguogu <momongo0913@g.ecc.u-tokyo.ac.jp>
2023-11-24 18:40:52 +0900

    send_word_to_gptにタイムリミットを追加

--- flash_card/lib/application/function/send_word_to_gpt.dart
+++ flash_card/lib/application/function/send_word_to_gpt.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -8,23 +9,25 @@ import 'package:flash_card/presentation/provider/provider_in_flash.dart';
 Future<void> sendWordToGPT(BuildContext context, WidgetRef ref,
     TextEditingController controller) async {
   try {
-    final response = await http.post(
-      Uri.parse('https://api.openai.com/v1/chat/completions'),
-      headers: {
-        'Authorization': 'Bearer $key',
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode({
-        "model": wordCreateModel2,
-        "messages": [
-          {"role": "system", "content": wordCreateSystem2},
-          {
-            "role": "user",
-            "content": "What does the word '${controller.text}' mean?",
-          }
-        ],
-      }),
-    );
+    final response = await http
+        .post(
+          Uri.parse('https://api.openai.com/v1/chat/completions'),
+          headers: {
+            'Authorization': 'Bearer $key',
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode({
+            "model": wordCreateModel2,
+            "messages": [
+              {"role": "system", "content": wordCreateSystem2},
+              {
+                "role": "user",
+                "content": "What does the word '${controller.text}' mean?",
+              }
+            ],
+          }),
+        )
+        .timeout(const Duration(seconds: 18));
 
     if (response.statusCode == 200) {
       final jsonResponse = jsonDecode(utf8.decode(response.body.codeUnits))
@@ -37,10 +40,16 @@ Future<void> sendWordToGPT(BuildContext context, WidgetRef ref,
       // Handle the error or set an error message
       ref.read(responseProvider.notifier).state =
           "Error: ${response.statusCode}";
+      ref.read(errorGptProvider.notifier).state = true;
     }
+  } on TimeoutException catch (_) {
+    // タイムアウトエラーの処理
+    ref.read(responseProvider.notifier).state = "Error: Request timed out";
+    ref.read(errorGptProvider.notifier).state = true;
   } catch (e) {
     // Handle the error or set an error message
     ref.read(responseProvider.notifier).state = "Error: $e";
+    ref.read(errorGptProvider.notifier).state = true;
   }
   final responseState = ref.watch(responseProvider);
 
